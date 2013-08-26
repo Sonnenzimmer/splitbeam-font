f23a154cc063b3db47956a0ffc72d55e8535d5b1
glender <sh@signalwerk.ch>
2013-08-26 19:28:01 +0200

    MOD: Syntax cleaning

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,38 +1,39 @@
+module.exports = function (grunt) {
 
-module.exports = function(grunt) {
+    // Project configuration to deploy the 
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
 
-  // Project configuration to deploy the 
-  grunt.initConfig({
-    pkg: grunt.file.readJSON('package.json'),
+        assemble: {
+            options: {
+                flatten: true,
+                removeHbsWhitespace: true,
+                partials: ['./build/doc/partials/*.hbs'],
+                data: ['./build/Splitbeam-Regular.json'],
+                ext: '.html'
+            },
 
-  	assemble: {
-  	  options: {
-  		flatten: true,
-  		removeHbsWhitespace: true,
-  		partials: ['./source/doc/partials/*.hbs'],
-  		data: ['./build/SplitBean-Regular.json'],
-  		ext: '.html'
-  	  },
+            // build the html for the testpage
+            pages: {
+                src: ['./build/doc/templates/*.hbs'],
+                dest: 'deploy/'
+            },
 
-      // build the html for the testpage
-  	  pages: {
-    		src: ['./source/doc/templates/*.hbs'],
-    		dest: 'deploy/'
-  	  },
+            // build the css for the font (to get the codepoints)
+            css: {
+                options: {
+                    ext: '.css'
+                },
+                src: ['./build/doc/templates/css/*.hbs'],
+                dest: 'deploy/css/'
+            }
 
-      // build the css for the font (to get the codepoints)
-      css: {
-        options: { ext: '.css' },
-        src: ['./source/doc/templates/css/*.hbs'],
-        dest: 'deploy/css/'
-      }
+        },
+    });
 
-  	},
-  });
+    grunt.loadNpmTasks('assemble');
 
-  grunt.loadNpmTasks('assemble');
-
-  // Default task(s).
-  grunt.registerTask('default', ['assemble']);
+    // Default task(s).
+    grunt.registerTask('default', ['assemble']);
 
 };
